c29549c8571afbe4d9050f18dab15c6f0c86efac
Ben Hutchison <ben.hutchison@rea-group.com>
2017-09-18 16:35:49 +1000

    progress towards insert mode loader

--- build.sbt
+++ build.sbt
@@ -7,5 +7,9 @@ scalaVersion := "2.12.2"
 libraryDependencies ++= Seq(
   "com.github.scopt" %% "scopt" % "3.7.0",
   "net.tixxit" %% "delimited-core" % "0.9.0",
-  "org.postgresql" % "postgresql" % "42.1.4"
+  "org.postgresql" % "postgresql" % "42.1.4",
+  "org.typelevel" %% "cats-core" % "1.0.0-MF",
+  "com.github.benhutchison" %% "mouse" % "0.10-MF"
 )
+
+scalacOptions += "-Ypartial-unification"
--- src/main/scala/multiload/Main.scala
+++ src/main/scala/multiload/Main.scala
@@ -1,12 +1,25 @@
 package multiload
 
-import java.io.{File, IOException}
+import java.io._
 import java.nio.file._
 import java.nio.file.attribute.BasicFileAttributes
+import java.sql.{Connection, DriverManager}
 
 import scala.collection.mutable.ListBuffer
+import net.tixxit.delimited._
+
+import scala.util.Try
+
+import cats._
+import cats.implicits._
+
+import mouse.all._
 
 object Main {
+
+  val ModeInsert = "insert"
+
+  val BatchSize = 1000
 
 
   val parser = new scopt.OptionParser[Config]("multiload") {
@@ -32,22 +45,108 @@ object Main {
         case Left(msg) => failure(msg)
       }).valueName(ColumnSpec.ColInputDescription).
       text("Specifies the name or index of a CSV column to be loaded." +
-        "To refer to input columns by Name, the CSV must have a header row." +
+        "To refer to mapping columns by Name, the CSV must have a header row." +
         "Indexes are specified using codes COL0, COL1.. . " +
-        "If a second arg is provided, the input will be loaded into the database column with that name. " +
-        "If a 3rd argumwent ")
+        "If a second arg is provided, the mapping will be loaded into the database column with that name. " +
+        "If a 3rd argument ")
+
+    opt[Unit]('h', "header").action((_, c) =>
+      c.copy(verbose = true) ).text("Interpret first row  of mapping CSV files as header")
 
     opt[Unit]('v', "verbose").action( (_, c) =>
-      c.copy(verbose = true) ).text("verbose output")
+      c.copy(verbose = true) ).text("verbose error output")
 
     help("help").text("prints this usage text")
 
-    cmd("insert").action( (_, c) => c.copy(mode = Insert) ).
-      text("insert rows into an existing table. Either column mapping are defined for each column, or " +
-        "the input CSV has a header row, and each column exists in the target table.")
+    cmd(ModeInsert).action( (_, c) => c.copy(mode = Insert) ).
+      text("insert rows into an existing table. Either column mapping are defined for each non-nullable column, or " +
+        "the mapping CSV has a header row, and each column exists in the target table.").
+      children(
+        opt[String]("tablename").required().action((x, c) =>
+          c.copy(tablename = x) ).text("Existing table to insert data into"),
+        opt[Boolean]("truncate").action( (x, c) =>
+          c.copy(truncate = x) ).text("truncate table before insert data, default=false."),
+      )
 
     note("some notes.")
 
+  }
+
+  def doInsertBatch(conn: Connection, columnMappings: Seq[ColumnSpec],
+                    batch: Seq[Either[DelimitedError, Row]], tablename: String, colSql: String): Either[String, JobProgress] = {
+
+    val st = conn.createStatement()
+    val result = for {
+      values <- batch.toList.traverse((tryRow: Either[DelimitedError, Row]) => tryRow.map(_.map(field => s"'$field'")).left.map(_.toString))
+      _ <- Try(st.addBatch(s"Insert into $tablename ($colSql) values(${values.mkString(", ")})")).toEither.left.map(_.toString)
+      okCount <- Try(st.executeBatch()).toEither.left.map(_.toString)
+    } yield (JobProgress(okCount.sum, 0))
+    st.close()
+    result
+  }
+
+  def columnListSql(conn: Connection, config: Config): String = {
+    config.columns.map {
+      case IndexToName(index, nameOut, _) => nameOut
+      case NameToName(nameIn, nameOut, _) => nameOut
+    }.mkString(" ,")
+  }
+
+  def getOrderedColumns(conn: Connection, tablename: String): Either[String, IndexedSeq[String]] = {
+    val colBuffer = new ListBuffer[(Int, String)]
+    Try {
+      val rs = conn.getMetaData().getColumns(null, null, tablename, null)
+      while(rs.next()) {
+        val colIdx = rs.getInt("ORDINAL_POSITION")
+        val colName = rs.getString("COLUMN_NAME")
+        colBuffer += (colIdx -> colName)
+      }
+      //ensure the columns are sorted so they'll line up against CSV
+      val (idxs, names) = colBuffer.toSeq.sorted.unzip
+      //Sanity check: ensure all indexes are present and exactly once
+      require(idxs.toList == 1.to(idxs.size).toList)
+      names.toIndexedSeq
+    }.toEither.left.map(_.toString)
+  }
+
+  def doInsertFile(conn: Connection, config: Config, path: Path): Either[String, JobProgress] = {
+    val (rowIter, releaseHandle) = rowsFrom(path)
+    val columnListSql1 = columnListSql(conn, config)
+
+    for {
+      optHeaders <- (config.header).option(rowIter.next().map(_.toIndexedSeq.zipWithIndex).left.map(_.toString())).sequence
+      optHeaders1: Option[IndexedSeq[(String, Int)]] = optHeaders
+      columnMappings <- (config.columns.nonEmpty, optHeaders1.isDefined) match {
+        case (true, true) =>
+          convertInputNamesToIndices(config.columns, optHeaders1)
+        case (false, true) =>
+          optHeaders1.toRight("Impossible").map(_.map {case(c, i) => IndexToName(i, c)})
+        case (true, false) =>
+          val nameMappings = config.columns.filter(_.isInstanceOf[NameToName])
+          if (nameMappings.nonEmpty)
+            Left(s"Column name mapping defined but no header")
+          else
+            Right(config.columns)
+        case (false, false) =>
+          Left(s"Error processing file $path. " +
+            s"Either at least one columns mapping (-c option) or an mapping file header (-h flag) must be provided.")
+      }
+      batchResults <- rowIter.grouped(BatchSize).toStream.traverse(
+        batch => doInsertBatch(conn, columnMappings, batch, config.tablename, columnListSql1))
+    } yield JobProgress(batchResults.map(_.success).sum, 0)
+  }
+
+  def convertInputNamesToIndices(colSpecs: Seq[ColumnSpec],
+                                 optHeader: Option[IndexedSeq[(String, Int)]]): Either[String, List[IndexToName]] = {
+
+    val namesToIndices = optHeader.map(_.toMap).toRight(s"Column mapping specifies named input but no input column header names.")
+    colSpecs.toList.traverse {
+      case NameToName(in, out, typ) => namesToIndices.flatMap(m =>
+        m.get(in).toRight(s"Input column $in not found in: ${m.keys.mkString(", ")}")).map(i =>
+        IndexToName(i, out, typ)
+      )
+      case in: IndexToName => Right(in)
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -55,8 +154,19 @@ object Main {
     parser.parse(args, Config()) match {
       case Some(config) =>
         println(s"valid config: $config")
+
         val filePaths = filesInGlobPath(config.files)
         println(s"Files in path: ${filePaths.mkString(",")}")
+
+        Class.forName("org.postgresql.Driver")
+        val conn = DriverManager.getConnection(config.jdbc, config.user, config.password)
+        conn.setAutoCommit(false)
+        println(s"Connected OK to: ${config.jdbc} as user: ${config.user}")
+
+        if (config.mode == ModeInsert)
+          for {
+            outcomes <- filePaths.traverse(doInsertFile(conn, config, _))
+          } yield (outcomes)
 
       case None =>
       // arguments are bad, error message will have been displayed
@@ -86,22 +196,38 @@ object Main {
     matchedFiles.toList
   }
 
+  def rowsFrom(csvFile: Path): (Iterator[Either[DelimitedError, Row]], Closeable) = {
+    val parser: DelimitedParser = DelimitedParser(DelimitedFormat.Guess)
+    val fr = new FileReader(csvFile.toFile)
+    val reader: Reader = new BufferedReader(fr)
+    (parser.parseReader(reader), fr)
+  }
+
 }
 
-case class ColumnSpec(input: InputCol, optOutputCol: Option[String] = None, sqlType: SqlType = StringType)
+
+
+case class JobProgress(success: Int, fail: Int) {
+
+  def total = success + fail
+}
+
+sealed trait ColumnSpec
+case class IndexToName(index: Int, name: String, sqlType: SqlType = StringType) extends ColumnSpec
+case class NameToName(name: String, outName: String, sqlType: SqlType = StringType) extends ColumnSpec
 object ColumnSpec {
-  val ColInputDescription = "<Column Name>|<COLn> [Name in table [SQL data type]]"
+  val ColInputDescription = "<mapping name> [<output name>] [<datatype>] | <'COLn' mapping index> <output name> [data type]]"
 
   val RegexTableName = "([0-9a-zA-Z$_]+)".r
+  val RegexColumnIndex = "COL(\\d+)".r
 
   def fromSeq(s: Seq[String]): Either[String, ColumnSpec] = s match {
-    case Seq(inputCol) =>
-      InputCol.parse(inputCol).map(new ColumnSpec(_))
-    case Seq(inputCol, RegexTableName(outputCol)) =>
-      InputCol.parse(inputCol).map(new ColumnSpec(_, Some(outputCol)))
-    case Seq(inputCol, RegexTableName(outputCol), typStr) =>
-      InputCol.parse(inputCol).flatMap(ic =>
-        SqlType.parse(typStr).map(sqlType => new ColumnSpec(ic, Some(outputCol), sqlType)))
+    case Seq(RegexTableName(n)) =>
+      Right(new NameToName(n, n))
+    case Seq(RegexColumnIndex(i), RegexTableName(n)) =>
+      Right(new IndexToName(i.toInt, n))
+    case Seq(RegexTableName(n), RegexTableName(o), typStr) =>
+        SqlType.parse(typStr).map(sqlType => new NameToName(n, o, sqlType))
     case _ => Left(s"Invalid column spec '${s.mkString(" ")}' doesnt match: $ColInputDescription")
   }
 }
@@ -116,28 +242,13 @@ object SqlType {
   }
 }
 
-sealed trait InputCol
-case class ColIndex(index: Int) extends InputCol
-case class ColName(name: String) extends InputCol
-object InputCol {
-  import ColumnSpec.RegexTableName
-  val RegexColumnIndex = "COL(\\d+)".r
-
-
-  def parse(raw: String): Either[String, InputCol] = raw match {
-    case RegexTableName(name) => Right(ColName(name))
-    case RegexColumnIndex(index) => Right(ColIndex(index.toInt))
-    case _ => Left(s"Invalid input column identifier doesn't match patterns for name ${RegexTableName} " +
-      s"or column ${RegexColumnIndex}")
-  }
-
-}
-
 
 sealed trait Mode
 case object Insert extends Mode
 
 case class Config(mode: Mode = Insert, files: String = "",
                   jdbc: String = "", user: String = "", password: String = "",
+                  tablename: String = "", truncate: Boolean = false,
                   columns: Seq[ColumnSpec] = Seq.empty,
+                  header: Boolean = false,
                   verbose: Boolean = false, quiet: Boolean = false)
